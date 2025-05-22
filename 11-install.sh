64e2c8d4113cf6ce32ed3ae0ef1263fd56069ff5
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-05-22 19:02:21 +0530

    finding root user both -eq and -ne input non root user

--- 11-install.sh
+++ 11-install.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+
 USERID=$(id -u)
+
+echo "using $USERID -eq 0  "
 if [ $USERID -eq 0 ]
 then 
-        echo "  root user "
+        echo "   root user "
 else :
     echo " not root user "
+fi
+
+
+echo "using $USERID -ne 0  "
+
+if [ $USERID -ne 0 ]
+then 
+        echo " not  root user "
+else :
+    echo "  root user "
 fi
