9f77a523ba2c741927f367e513f9a59ab71d8e92
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-05-22 17:43:37 +0530

    converstion  03 svm

---
+++ 03-newcov.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=maha
+person2=srivenkata
+
+echo " $person1 : hi  srivaru:"
+echo " $person2: hi $person1  :"
+echo " $person1 : can u pls bless with nijapada darshanam :"
+echo " $person2: sure $person1 , I will bless , be ready for darshanam:"
