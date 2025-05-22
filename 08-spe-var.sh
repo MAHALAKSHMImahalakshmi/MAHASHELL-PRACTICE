115bfc07738aae3930571725776995deefcb71db
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-05-22 18:10:31 +0530

    arrays

---
+++ 08-spe-var.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ 
+ dasanama=("govinda" "ranga" "rama" "srinivas" "venkata" "srivaru" "jagadananda" "krishna" "bujjibangaram" "sarvam")
+
+ echo "first nama of venkataeshwaraswamy :$dasanama[0]"
+ echo "first nama of venkataeshwaraswamy :${dasanama[0]}"
+ echo "second nama of venkataeshwaraswamy :${dasanama[1]}"
+ echo "all dasa nama of venkataeshwaraswamy :${dasanama[0]}"
