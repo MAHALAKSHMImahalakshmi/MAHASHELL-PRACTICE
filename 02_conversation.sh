41c4a8a8b4c8581b4444f7e91b17a9dd6cc0035d
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-05-22 17:38:29 +0530

    converstion  svm

---
+++ 02_conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo " Maha : hi  srivaru:"
+echo " Srivenkata : hi maha  :"
+echo " Maha : can u pls bless with nijapada darshanam :"
+echo " srivenkata : sure maha , I will bless , be ready for darshanam:"
