981e3b32d0157c60e8903c062d300a6317202837
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-06-05 17:08:58 +0530

    delete log file

--- 21-backup.sh
+++ 21-backup.sh
@@ -13,7 +13,7 @@ Y="\e[33m"
 N="\e[0m"
 
 LOG_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=${echo $0 | cut -d "." -f1}
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOG_FOLDER
